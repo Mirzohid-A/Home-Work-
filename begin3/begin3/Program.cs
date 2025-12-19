78ae560583581224e35d309df420b3f40655c54a
Mirzohid-A <dropxsola@gmail.com>
2025-12-19 18:34:47 +0500

    Add files via upload

---
+++ begin3/begin3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+ namespace begin3
+{
+     class Program 
+      {
+        static void Main()
+        {
+            Console.WriteLine("Строны прямаугольника, a =?");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите вторую сторону прямоугольника b=?");
+            int b = int.Parse(Console.ReadLine());
+            int s = a * b;
+            int p =2 * (a + b);
+            Console.WriteLine($"Площадь прямаугольника  = {s}, Периметр = {p}");
+        }  
+      }
+}
