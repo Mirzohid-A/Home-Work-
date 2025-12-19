18ee12feb70ea79a1cd2316e342894fc630ef0a1
Mirzohid-A <dropxsola@gmail.com>
2025-12-19 18:37:29 +0500

    Add files via upload

---
+++ Begin5/Begin5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Begin5
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("длина ребра куба a=?");
+            int a = Convert.ToInt32(Console.ReadLine());
+            var v = Math.Pow(a, 3);
+            var s = 6 * Math.Pow(a, 2);
+            Console.WriteLine("объем куба v=" + v + "площадь поверхности s=" + s);
+        }
+    }
+}
