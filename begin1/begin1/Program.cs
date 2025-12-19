312f9d8526b41d8faf40ab6d3e49ab0cc204af40
Mirzohid-A <dropxsola@gmail.com>
2025-12-19 18:27:25 +0500

    Add files via upload

---
+++ begin1/begin1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+   namespace begin1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("сторона квадрата =?");
+            int p = Convert.ToInt32(Console.ReadLine());
+            int x = 4*p;
+            Console.Write($"периметр {x} ");
+        }
+    }
+}
