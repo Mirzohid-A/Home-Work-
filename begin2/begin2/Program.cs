7a7e80a2cfa90ec5f2db2dd0b532066a00e94497
Mirzohid-A <dropxsola@gmail.com>
2025-12-19 18:33:49 +0500

    Add files via upload

---
+++ begin2/begin2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+    namespace begin2
+{
+    class Program 
+    {
+    static void Main()
+        {
+            Console.WriteLine("сторона квадрата a=?");
+             double a = double.Parse(Console.ReadLine());
+             double s = Math.Pow(a, 2);
+            Console.WriteLine($"Площадь квадрата со стороной {a} = {s}");
+        }
+        
+    }
+}
