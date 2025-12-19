78c8cd036a9d25413ed917141655a7b6343a6c52
Mirzohid-A <dropxsola@gmail.com>
2025-12-19 18:39:32 +0500

    Add files via upload

---
+++ Begin6/Begin6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Begin6
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите длины ребер a=?");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите длиный ребер b=?");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите длины ребер с=?");
+            int c = Convert.ToInt32(Console.ReadLine());
+            var v = a * b * c;
+            var s = 2 * (a * b + b * c + a * c);
+            Console.WriteLine("Объем параллелепипеда v=" + v);
+            Console.WriteLine("Площадь поверхности s =" + s);
+        }
+    }
+}
