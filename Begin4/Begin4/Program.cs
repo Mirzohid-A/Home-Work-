cea6525cee9bab12ddaa3720e4bd6b00636318a1
Mirzohid-A <dropxsola@gmail.com>
2025-12-19 18:36:01 +0500

    Add files via upload

---
+++ Begin4/Begin4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+ namespace Begin4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(" Диаметр =?");
+            int d = Convert.ToInt32(Console.ReadLine());
+            double l = Math.PI * d;
+           
+            Console.WriteLine($"Диаметр d = {d} \nPi = 3.14, \nответ = {l} ");
+        }
+    }
+}s
