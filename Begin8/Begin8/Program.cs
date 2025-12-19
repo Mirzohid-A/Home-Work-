f4f3debe17f87b8d7b37cc330a02dd649cf0c67b
Mirzohid-A <dropxsola@gmail.com>
2025-12-19 18:45:18 +0500

    Add files via upload

---
+++ Begin8/Begin8/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+ namespace Begin8
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("a=?");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("b=?");
+            int b = Convert.ToInt32(Console.ReadLine());
+            var s = (a + b) / 2;
+            Console.WriteLine("среднее арифметическое" + s);
+        }
+    }
+}
---
+++ Begin9/Begin9/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+   namespace Begin9
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите число A:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите числло B:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            var s = Math.Sqrt(a * b);
+            Console.WriteLine("Среднее геометрическое чисел A и B равно: " + s);
+        }
+    }
+}
