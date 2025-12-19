23271e8a0b167e4889ceca51aa904771472a092f
Mirzohid-A <dropxsola@gmail.com>
2025-12-19 19:03:31 +0500

    Add files via upload

---
+++ integer24/integer24/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+namespace integer
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Мохро бо бо ракам ворид намоед:");
+            int sol = Convert.ToInt32(Console.ReadLine());
+
+            if (sol == 1)
+            {
+                Console.WriteLine("Январь");
+            }
+
+            else if (sol == 2)
+            {
+                Console.WriteLine("Февраль");
+            }
+
+            else if (sol == 3)
+
+            {
+                Console.WriteLine("Март");
+            }
+
+            else if (sol == 4)
+            {
+                Console.WriteLine("Апрель");
+
+            }
+            else if (sol == 5)
+
+            {
+                Console.WriteLine("Май");
+            }
+            else if (sol == 6)
+            {
+                Console.WriteLine("Июнь");
+            }
+            else if (sol == 7)
+            {
+                Console.WriteLine("Июль");
+            }
+            else if (sol == 8)
+            {
+                Console.WriteLine("Август");
+            }
+            else if (sol == 9)
+            {
+                Console.WriteLine("Сентябрь");
+            }
+            else if (sol == 10)
+            {
+                Console.WriteLine("Октябрь");
+            }
+            else if (sol == 11)
+            {
+                Console.WriteLine("Ноябрь");
+            }
+            else if (sol == 12)
+            {
+                Console.WriteLine("Декабрь");
+            }
+            else
+            {
+                Console.WriteLine("Интихоби шумо нодуруст аст!!!");
+            };
+
+            //foreach(var m in sol)
+            //{
+              
+           // }
+
+
+        }
+    }
+}
---
+++ intejer/intejer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace intejer
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Дарозиро бо сантиметр ворид кунед:");
+            int l = Convert.ToInt32(Console.ReadLine());
+            int metr = l / 100;
+            Console.WriteLine("Дарози дар метр:" + metr);
+        }
+    }
+}
