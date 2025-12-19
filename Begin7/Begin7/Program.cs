46779c90967a9bc584349ed39a5b850d47e3ffd0
Mirzohid-A <dropxsola@gmail.com>
2025-12-19 18:42:59 +0500

    Add files via upload

---
+++ Begin7/Begin7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Begin7
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(" R окружности =?");
+            double r = Convert.ToDouble(Console.ReadLine());
+            var l = 2 * Math.PI * r;
+            var s = Math.PI * Math.Pow(r, 2);
+            Console.WriteLine("Длина окружоности L=" + l + "и Площадь S=" + s);
+        }
+    }
+}
