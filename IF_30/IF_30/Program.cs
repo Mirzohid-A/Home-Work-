b2ea68b2452ea39af1c634047dc2113f95a0d8f1
Mirzohid-A <dropxsola@gmail.com>
2025-12-22 19:56:13 +0500

    Add files via upload

---
+++ IF_30/IF_30/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ададро аз 1 то 999 ворид кунед: ");
+        int number = int.Parse(Console.ReadLine());
+
+        string psN;
+        if (number % 2 == 0)
+            psN = "чуфт";
+        else
+            psN = "ток";
+
+        int digits = number.ToString().Length;
+        string raqam;
+        if (digits == 1)
+            raqam = "як ракама";
+        else if (digits == 2)
+            raqam = "ду ракама";
+        else
+            raqam = "се ракама";
+
+        Console.WriteLine($"Адади {psN} {raqam}");
+    }
+}
